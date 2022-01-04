3251e717b4a16125692ba5dfeb9c94f61c778d84
dnnzz <denizfiratt03@gmail.com>
2022-01-04 23:49:38 +0300

    resultpagelist done & some css fixes

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,12 +3,12 @@ import styles from './ListItem.module.css';
 export default function ListItem() {
     return (
         <div className={styles.listItem}>
-            <div>
+            <div className={styles.userInfo}>
                 <p className={styles.city}>Turkey - Istanbul</p>
                 <p className={styles.name}>Jane Doe - 2016</p>
             </div>
             <div className={styles.userEmailInfo}>
-                <p className={styles.userEmail}>Email: abc@xyz.com</p>
+                <p className={styles.userEmail}>Email: Praesent.luctus.Curabitur@SuspendisseduiFusce.ca</p>
             </div>
         </div>
     )
--- src/components/ResultPageList/ResultPageList.jsx
+++ src/components/ResultPageList/ResultPageList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import styles from './ResultPageList.module.css';
+import ListItem from '../ListItem/ListItem';
+import icon from '../../assets/icon.png';
 export default function ResultPageList() {
     return (
-        <div>
-            
+        <div className={styles.pageListWrapper}>
+            <div className={styles.orderSection}>
+                <img src={icon}  alt="icon" />
+                <button className={styles.btn}>Order By</button>
+            </div>
+            <ListItem />
+            <ListItem />
+            <ListItem />
         </div>
     )
 }
