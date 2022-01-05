42dc2e149099544ee119daebfdbb170c35958f7e
dnnzz <denizfiratt03@gmail.com>
2022-01-05 14:28:15 +0300

    feat/pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import styles from './Pagination.module.css';
-export default function Pagination() {
+export default function Pagination({userPerPage,paginate,allUsers}) {
+    const pageNumbers = [];
+    const [currentNumber, setCurrentNumber] = React.useState(1);
+    for (let i = 1; i <= Math.ceil(allUsers / userPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    if (pageNumbers.length < 2) {
+        return null;
+    }
     return (
         <div className={styles.pagination}>
-            <button className={styles.previous}>Previous</button>
-            <button className={styles.buttons}>1</button>
-            <button className={styles.buttons}>1</button>
-            <button className={styles.buttons}>1</button>
-            <button className={styles.buttons}>1</button>
-            <button className={styles.next}>Next</button>         
+            <button 
+            onClick={() => {
+                if (currentNumber > 1) {
+                  setCurrentNumber(currentNumber - 1);
+                  paginate(currentNumber - 1);
+                } else {
+                  return;
+                }
+              }}
+            className={styles.previous}>Previous</button>
+            {
+                pageNumbers.map((num) =>(
+                    <button 
+                    key={num}
+                    onClick={() => {
+                        setCurrentNumber(num);
+                        paginate(num);
+                      }}
+                    className={styles.buttons}>{num}</button>
+                ))
+            }
+            <button 
+             onClick={() => {
+                if (currentNumber < pageNumbers.length) {
+                  setCurrentNumber(currentNumber + 1);
+                  paginate(currentNumber + 1);
+                }
+              }}
+            className={styles.next}>Next</button>         
         </div>
     )
 }
--- src/components/ResultPageList/ResultPageList.jsx
+++ src/components/ResultPageList/ResultPageList.jsx
@@ -3,8 +3,19 @@ import styles from './ResultPageList.module.css';
 import ListItem from '../ListItem/ListItem';
 import Pagination from '../Pagination/Pagination';
 import icon from '../../assets/icon.png';
+import { useUserContext } from '../../context/Context';
 export default function ResultPageList() {
+    const {
+        state
+    } = useUserContext();
     const [openMenu,setMenu] = React.useState(false);
+    const [currentPage, setCurrentPage] = React.useState(1);
+
+    const indexOfLastUser = currentPage * 6;
+    const indexOfFirstUser = indexOfLastUser - 6;
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+      };
     return (
         <div className={styles.pageListWrapper}>
             <div className={styles.orderSection}>
@@ -19,10 +30,24 @@ export default function ResultPageList() {
                 </div>
                 }
             </div>
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <Pagination /> 
+            {
+                state.filteredUsers && state.filteredUsers.map((i,index)=>
+                i.slice(indexOfFirstUser, indexOfLastUser).map(
+                    (user)=>
+                    <ListItem 
+                    key={`${index}-${user[0]}`} 
+                    name={user[0]}
+                    email={user[2]}
+                    date={user[3]}
+                    country={user[4]}
+                    state={user[5]} />
+                    ))
+            }
+            <Pagination 
+            userPerPage={6}
+            allUsers={state.filteredUsers[0].length}
+            paginate={paginate}
+            /> 
         </div>
     )
 }
