339b7c0e9601cd27058b9d17c26f2dc483b3f514
dnnzz <denizfiratt03@gmail.com>
2022-01-05 13:45:41 +0300

    fix/css && feat/listpagesearch

--- src/components/ResultPageHeader/ResultPageHeader.jsx
+++ src/components/ResultPageHeader/ResultPageHeader.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import logo from '../../assets/logo.jpeg';
 import styles from './ResultPageHeader.module.css';
+import { useUserContext } from "../../context/Context";
 export default function ResultPageHeader() {
+    const { dispatch } = useUserContext();
+    const [query, setQuery] = React.useState("");
     return (
         <div className={styles.header}>
         <img src={logo} className={styles.logo} alt="tesodev-logo" /> 
-        <input className={styles.resultPageInput} type="text" />
-        <button className={styles.resultPageBtn}>Search</button>
+        <input className={styles.resultPageInput} 
+        onChange={(e) => setQuery(e.target.value)}
+        type="text" />
+        <button
+        onClick={() =>
+            dispatch({ type: "SEARCH_USERS", payload: query })
+        }
+        className={styles.resultPageBtn}>Search</button>
     </div>
     )
 }
